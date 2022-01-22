45209627fa26fe673bcd6ac3a6f42413d93649b2
tor.helge.skei <tor.helge.skei@gmail.com>
2022-01-22 18:03:33 +0100

    initial lv2 entry

--- include/plugin/lv2/mip_lv2.h
+++ include/plugin/lv2/mip_lv2.h
@@ -4,9 +4,25 @@
 
 #include "mip.h"
 
+#include <lv2.h>
+#include <lv2/atom/atom.h>
+#include "lv2/atom/util.h"
+#include <lv2/core/lv2.h>
+#include "lv2/core/lv2_util.h"
+#include "lv2/log/log.h"
+#include "lv2/log/logger.h"
+#include <lv2/midi/midi.h>
+#include <lv2/urid/urid.h>
+
+#include <lv2/lv2plug.in/ns/extensions/ui/ui.h>
+
+//----------------------------------------------------------------------
+
+#define MIP_LV2_MAX_URI_LENGTH     256
+#define MIP_LV2_MANIFEST_TTL_SIZE  65536
+#define MIP_LV2_PLUGIN_TTL_SIZE    65536
+
+#define MIP_LV2_EXPORT_TTL
 
 //----------------------------------------------------------------------
 #endif
-
-
-
--- include/plugin/lv2/mip_lv2_entry.h
+++ include/plugin/lv2/mip_lv2_entry.h
@@ -3,6 +3,62 @@
 //----------------------------------------------------------------------
 
 #include "mip.h"
+#include "plugin/lv2/mip_lv2.h"
+
+//----------------------------------------------------------------------
+//
+//
+//
+//----------------------------------------------------------------------
+
+class MIP_Lv2PluginEntry {
+
+//------------------------------
+public:
+//------------------------------
+
+  MIP_Lv2PluginEntry() {
+    MIP_PRINT;
+  }
+
+  //----------
+
+  virtual ~MIP_Lv2PluginEntry() {
+    MIP_PRINT;
+  }
+
+//------------------------------
+public:
+//------------------------------
+
+  const LV2_Descriptor* entry(unsigned long Index) {
+    MIP_PRINT;
+    return nullptr;
+  }
+
+};
+
+//----------------------------------------------------------------------
+//
+//
+//
+//----------------------------------------------------------------------
+
+const LV2_Descriptor* mip_lv2_entry(  uint32_t Index) asm ("lv2_descriptor");
+//const LV2UI_Descriptor* mip_lv2ui_entrypoint(uint32_t index) asm ("lv2ui_descriptor"); // MIP_LV2_UI_SYMBOL
+
+//----------
+
+MIP_Lv2PluginEntry GLOBAL_LV2_PLUGIN_ENTRY;
+
+//----------
+
+//__MIP_EXPORT
+__attribute__ ((visibility ("default")))
+const LV2_Descriptor* mip_lv2_entry(unsigned long Index) {
+  MIP_PRINT;
+  return GLOBAL_LV2_PLUGIN_ENTRY.entry(Index);
+}
 
 //----------------------------------------------------------------------
 #endif
