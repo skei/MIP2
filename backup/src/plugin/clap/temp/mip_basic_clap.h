0e9a28e965b455e20f81b400bb0f6acbf24e4df8
tor.helge.skei <tor.helge.skei@gmail.com>
2022-01-27 00:47:12 +0100

    moved backup files

--- backup/src/plugin/clap/temp/mip_basic_clap.h
+++
@@ -1,9 +0,0 @@
-#ifndef mip_basic_clap_included
-#define mip_basic_clap_included
-//----------------------------------------------------------------------
-
-#include "mip.h"
-#include "plugin/clap/mip_clap.h"
-
-//----------------------------------------------------------------------
-#endif
