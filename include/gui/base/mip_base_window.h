745ff5800c401e507e68d083a962898088e494aa
tor.helge.skei <tor.helge.skei@gmail.com>
2022-02-06 03:40:09 +0100

    ...

--- include/gui/base/mip_base_window.h
+++ include/gui/base/mip_base_window.h
@@ -30,6 +30,26 @@ public:
 public:
 //------------------------------
 
+  //virtual void on_window_open() {}
+  //virtual void on_window_close() {}
+  virtual void on_window_move(int32_t AXpos, int32_t AYpos) {}
+  virtual void on_window_resize(int32_t AWidth, int32_t AHeight) {}
+  virtual void on_window_keyPress(uint32_t AKey, uint32_t AState, uint32_t ATimeStamp) {}
+  virtual void on_window_keyRelease(uint32_t AKey, uint32_t AState, uint32_t ATimeStamp) {}
+  virtual void on_window_mouseClick(int32_t AXpos, int32_t AYpos, uint32_t AButton, uint32_t AState, uint32_t ATimeStamp) {}
+  virtual void on_window_mouseRelease(int32_t AXpos, int32_t AYpos, uint32_t AButton, uint32_t AState, uint32_t ATimeStamp) {}
+  virtual void on_window_mouseMove(int32_t AXpos, int32_t AYpos, uint32_t AState, uint32_t ATimeStamp) {}
+  virtual void on_window_mouseEnter(int32_t AXpos, int32_t AYpos, uint32_t ATimeStamp) {}
+  virtual void on_window_mouseLeave(int32_t AXpos, int32_t AYpos, uint32_t ATimeStamp) {}
+  virtual void on_window_timer() {}
+  virtual void on_window_idle() {}
+  virtual void on_window_clientMessage(uint32_t AData, void* APtr) {}
+  virtual void on_window_paint(int32_t AXpos, int32_t AYpos, int32_t AWidth, int32_t AHeight) {}
+
+//------------------------------
+public:
+//------------------------------
+
   //virtual MIP_Painter* getPainter() { return nullptr; }
 
   virtual void setWindowPos(uint32_t AXpos, uint32_t AYpos) {}
@@ -60,26 +80,6 @@ public:
   virtual void fill(int32_t AXpos, int32_t AYpos, int32_t AWidth, int32_t AHeight, MIP_Color AColor) {}
   virtual void blit(int32_t ADstX, int32_t ADstY, MIP_Drawable* ASource) {}
   virtual void blit(int32_t ADstX, int32_t ADstY, MIP_Drawable* ASource, int32_t ASrcX, int32_t ASrcY, int32_t ASrcW, int32_t ASrcH) {}
-
-//------------------------------
-public:
-//------------------------------
-
-  //virtual void on_window_open() {}
-  //virtual void on_window_close() {}
-  virtual void on_window_move(int32_t AXpos, int32_t AYpos) {}
-  virtual void on_window_resize(int32_t AWidth, int32_t AHeight) {}
-  virtual void on_window_keyPress(uint32_t AKey, uint32_t AState, uint32_t ATimeStamp) {}
-  virtual void on_window_keyRelease(uint32_t AKey, uint32_t AState, uint32_t ATimeStamp) {}
-  virtual void on_window_mouseClick(int32_t AXpos, int32_t AYpos, uint32_t AButton, uint32_t AState, uint32_t ATimeStamp) {}
-  virtual void on_window_mouseRelease(int32_t AXpos, int32_t AYpos, uint32_t AButton, uint32_t AState, uint32_t ATimeStamp) {}
-  virtual void on_window_mouseMove(int32_t AXpos, int32_t AYpos, uint32_t AState, uint32_t ATimeStamp) {}
-  virtual void on_window_mouseEnter(int32_t AXpos, int32_t AYpos, uint32_t ATimeStamp) {}
-  virtual void on_window_mouseLeave(int32_t AXpos, int32_t AYpos, uint32_t ATimeStamp) {}
-  virtual void on_window_timer() {}
-  virtual void on_window_idle() {}
-  virtual void on_window_clientMessage(uint32_t AData, void* APtr) {}
-  virtual void on_window_paint(int32_t AXpos, int32_t AYpos, int32_t AWidth, int32_t AHeight) {}
 
 };
 
