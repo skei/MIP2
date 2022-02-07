77fd4874f5b5fa8f7995223b1526c3b36d835ac6
tor.helge.skei <tor.helge.skei@gmail.com>
2022-02-07 23:36:51 +0100

    minor reorg.

--- build/build.cpp
+++ build/build.cpp
@@ -17,13 +17,16 @@
 //#include "../source/plugins/clap-minimal.cpp"
 //#include "../source/plugins/clap-gain.cpp"
 
+#include "../source/plugins/test_plugin1.cpp"
+
+
 //----------------------------------------------------------------------
 //
 // test
 //
 //----------------------------------------------------------------------
 
-#include "../source/test/test.cpp"
+//#include "../source/test/test.cpp"
 
 //#include "../source/test/test_gui.cpp"
 //#include "../source/test/test_plugin.cpp"
