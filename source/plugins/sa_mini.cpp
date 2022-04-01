62e130816e75eb0a87256fd6ebcd7c7f5ef07722
tor.helge.skei <tor.helge.skei@gmail.com>
2022-04-01 18:56:20 +0200

    added midi transpose example plugin

--- source/plugins/sa_mini.cpp
+++ source/plugins/sa_mini.cpp
@@ -6,9 +6,10 @@
 
 #include "plugin/mip_plugin.h"
 
+#include "sa_mini/sa_fibonacci.h"
 #include "sa_mini/sa_grains.h"
 #include "sa_mini/sa_pitch.h"
-#include "sa_mini/sa_fibonacci.h"
+#include "sa_mini/sa_transpose.h"
 
 //----------------------------------------------------------------------
 //
@@ -17,9 +18,10 @@
 //----------------------------------------------------------------------
 
 void MIP_Register(MIP_ClapRegistry* ARegistry) {
+  ARegistry->appendPlugin(&sa_fibonacci_descriptor);
   ARegistry->appendPlugin(&sa_grains_descriptor);
   ARegistry->appendPlugin(&sa_pitch_descriptor);
-  ARegistry->appendPlugin(&sa_fibonacci_descriptor);
+  ARegistry->appendPlugin(&sa_transpose_descriptor);
 }
 
 //----------
@@ -31,9 +33,10 @@ void MIP_Register(MIP_ClapRegistry* ARegistry) {
 
 MIP_ClapPlugin* MIP_CreatePlugin(uint32_t AIndex, const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost) {
   switch (AIndex) {
-    case 0:  return new sa_grains_plugin(ADescriptor,AHost);
-    case 1:  return new sa_pitch_plugin(ADescriptor,AHost);
-    case 2:  return new sa_fibonacci_plugin(ADescriptor,AHost);
+    case 0:  return new sa_fibonacci_plugin(ADescriptor,AHost);
+    case 1:  return new sa_grains_plugin(ADescriptor,AHost);
+    case 2:  return new sa_pitch_plugin(ADescriptor,AHost);
+    case 3:  return new sa_transpose_plugin(ADescriptor,AHost);
   }
   return nullptr;
 }
