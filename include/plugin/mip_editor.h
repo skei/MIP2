299617b2049273a23d5b6f00e8f7845ee0422f4d
tor.helge.skei <tor.helge.skei@gmail.com>
2022-07-07 01:41:50 +0200

    editor api follows clap gui extension

--- include/plugin/mip_editor.h
+++ include/plugin/mip_editor.h
@@ -3,6 +3,7 @@
 //----------------------------------------------------------------------
 
 #include "mip.h"
+#include "plugin/clap/mip_clap.h"
 
 //----------------------------------------------------------------------
 
@@ -25,6 +26,9 @@ private:
 //------------------------------
 
   MIP_EditorListener* MListener = nullptr;
+  uint32_t            MWidth    = 640;
+  uint32_t            MHeight   = 480;
+  double              MScale    = 1.0;
 
 //------------------------------
 public:
@@ -45,6 +49,61 @@ public:
 
   void updateParameter(uint32_t AIndex, double AValue, bool ARedraw=true) {
     // if index -> connected.. set value, redraw
+  }
+
+//------------------------------
+public:
+//------------------------------
+
+  virtual bool set_scale(double scale) {
+    MScale = scale;
+    return true;
+  }
+
+  virtual bool get_size(uint32_t *width, uint32_t *height) {
+    *width = MWidth;
+    *height = MHeight;
+    return true;
+  }
+
+  virtual bool can_resize() {
+    return false;
+  }
+
+  virtual bool get_resize_hints(clap_gui_resize_hints_t *hints) {
+    hints->can_resize_horizontally  = false;
+    hints->can_resize_vertically    = false;
+    hints->aspect_ratio_width       = 1.0;
+    hints->aspect_ratio_height      = 1.0;
+    hints->preserve_aspect_ratio    = true;
+    return false;
+  }
+
+  virtual bool adjust_size(uint32_t *width, uint32_t *height) {
+    return false;
+  }
+
+  virtual bool set_size(uint32_t width, uint32_t height) {
+    return false;
+  }
+
+  virtual bool set_parent(const clap_window_t *window) {
+    return false;
+  }
+
+  virtual bool set_transient(const clap_window_t *window) {
+    return false;
+  }
+
+  virtual void suggest_title(const char *title) {
+  }
+
+  virtual bool show() {
+    return false;
+  }
+
+  virtual bool hide() {
+    return false;
   }
 
 };
