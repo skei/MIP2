32c264eaea646a4b272faa34001d76855b6593e3
tor.helge.skei <tor.helge.skei@gmail.com>
2022-07-21 01:28:24 +0200

    template plugin now has editor

--- source/plugins/template.cpp
+++ source/plugins/template.cpp
@@ -1,25 +1,26 @@
 
-//#define MIP_NO_ENTRY
+#define MIP_GUI_XCB
+#define MIP_PAINTER_NANOVG
 
 #define MIP_DEBUG_PRINT_SOCKET
 // nc -U -l -k /tmp/mip.socket
 
 //----------------------------------------------------------------------
 
-#include "plugin/clap/mip_clap.h"
 #include "plugin/mip_plugin.h"
-
+#include "plugin/mip_editor.h"
+#include "gui/widgets/mip_widgets.h"
 
 //----------------------------------------------------------------------
 //
-//
+// descriptor
 //
 //----------------------------------------------------------------------
 
-const clap_plugin_descriptor_t template_descriptor = {
+const clap_plugin_descriptor_t myDescriptor = {
    .clap_version  = CLAP_VERSION,
-   .id            = "me/template/0",
-   .name          = "template",
+   .id            = "me/myPlugin/0",
+   .name          = "myPlugin",
    .vendor        = "me",
    .url           = "https://my_website.com",
    .manual_url    = "",
@@ -29,44 +30,43 @@ const clap_plugin_descriptor_t template_descriptor = {
    .features      =  (const char *[]){ CLAP_PLUGIN_FEATURE_AUDIO_EFFECT, nullptr }
 };
 
-//----------
-
-//enum myParameterEnums {
-//  MY_PARAM1 = 0,
-//  MY_PARAM2,
-//  MY_PARAM3,
-//  MY_PARAM_COUNT
-//};
-//
-//const clap_param_info_t myParameters[MY_PARAM_COUNT] = {
-//  { MY_PARAM1, CLAP_PARAM_IS_AUTOMATABLE, nullptr, "param1", "", 0, 1, 0 },
-//  { MY_PARAM2, CLAP_PARAM_IS_AUTOMATABLE, nullptr, "param2", "", -1, 1, 1 },
-//  { MY_PARAM3, CLAP_PARAM_IS_AUTOMATABLE | CLAP_PARAM_IS_STEPPED, nullptr, "param3", "", -12, 12, 0 }
-//};
-//
-//const clap_audio_port_info_t myAudioInputPorts[] = {
-//  { 0, "audio in 1", CLAP_AUDIO_PORT_IS_MAIN, 2, CLAP_PORT_STEREO, CLAP_INVALID_ID }
-//};
-//
-//const clap_audio_port_info_t myAudioOutputPorts[] = {
-//  { 0, "audio out 1", CLAP_AUDIO_PORT_IS_MAIN, 2, CLAP_PORT_STEREO, CLAP_INVALID_ID }
-//};
-//
-//const clap_note_port_info_t myNoteInputPorts[] = {
-//  { 0, CLAP_NOTE_DIALECT_CLAP, CLAP_NOTE_DIALECT_CLAP, "note in 1" }
-//};
-//
-//const clap_note_port_info_t myNoteOutputPorts[] = {
-//  { 0, CLAP_NOTE_DIALECT_CLAP, CLAP_NOTE_DIALECT_CLAP, "note out 1" }
-//};
-
 //----------------------------------------------------------------------
 //
-//
+// editor
 //
 //----------------------------------------------------------------------
 
-class template_plugin
+class myEditor
+: public MIP_Editor {
+
+//------------------------------
+public:
+//------------------------------
+
+  myEditor(MIP_EditorListener* AListener, uint32_t AWidth, uint32_t AHeight)
+  : MIP_Editor(AListener,AWidth,AHeight) {
+    setWindowFillBackground(false);
+
+    MIP_ColorWidget* background = new MIP_ColorWidget( MIP_DRect( 0,0,500,400), MIP_COLOR_GRAY );
+    appendChildWidget(background);
+    background->Layout.alignment = MIP_WIDGET_ALIGN_CLIENT;
+
+  }
+
+  //----------
+
+  virtual ~myEditor() {
+  }
+
+};
+
+//----------------------------------------------------------------------
+//
+// gui
+//
+//----------------------------------------------------------------------
+
+class myPlugin
 : public MIP_Plugin {
 
 //------------------------------
@@ -106,13 +106,15 @@ private:
 public:
 //------------------------------
 
-  template_plugin(const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost)
+  myPlugin(const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost)
   : MIP_Plugin(ADescriptor,AHost) {
+    MEditorWidth = 300;
+    MEditorHeight = 300;
   }
 
   //----------
 
-  virtual ~template_plugin() {
+  virtual ~myPlugin() {
   }
 
 //------------------------------
@@ -130,13 +132,8 @@ public: // plugin
     return true;
   }
 
-  //bool activate(double sample_rate, uint32_t min_frames_count, uint32_t max_frames_count) final {
-  //  return true;
-  //}
-
-  //bool start_processing() final {
-  //  return true;
-  //}
+  //bool activate(double sample_rate, uint32_t min_frames_count, uint32_t max_frames_count) final {}
+  //bool start_processing() final {}
 
   //----------
 
@@ -144,6 +141,7 @@ public: // plugin
   //void postProcessEvents(const clap_input_events_t* in_events, const clap_output_events_t* out_events) final {}
   //void processEvents(const clap_input_events_t* in_events, const clap_output_events_t* out_events) final {}
   //void processEvent(const clap_event_header_t* header) final {}
+
   //void processNoteOnEvent(const clap_event_note_t* event) final {}
   //void processNoteOffEvent(const clap_event_note_t* event) final {}
   //void processNoteChokeEvent(const clap_event_note_t* event) final {}
@@ -158,6 +156,8 @@ public: // plugin
   //void processMidiSysexEvent(const clap_event_midi_sysex_t* event) final {}
   //void processMidi2Event(const clap_event_midi2_t* event) final {}
 
+  //----------
+
   //void processAudioBlock(const clap_process_t* process) final {}
   //void processTransport(const clap_event_transport_t* transport) final {}
 
@@ -165,16 +165,16 @@ public: // plugin
 public: // gui
 //------------------------------
 
-  //bool gui_create(const char *api, bool is_floating) override {
-  //  MEditor = new myEditor(this,MEditorWidth,MEditorHeight);
-  //  return true;
-  //}
+  bool gui_create(const char *api, bool is_floating) override {
+    MEditor = new myEditor(this,MEditorWidth,MEditorHeight);
+    return true;
+  }
 
   //----------
 
-  //void gui_destroy() override {
-  //  delete MEditor;
-  //}
+  void gui_destroy() override {
+    delete MEditor;
+  }
 
 };
 
@@ -192,14 +192,14 @@ public: // gui
   //----------
 
   void MIP_Register(MIP_Registry* ARegistry) {
-    ARegistry->appendDescriptor(&template_descriptor);
+    ARegistry->appendDescriptor(&myDescriptor);
   };
 
   //----------
 
   MIP_Plugin* MIP_CreatePlugin(uint32_t AIndex, const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost) {
     if (AIndex == 0) {
-      return new template_plugin(ADescriptor,AHost);
+      return new myPlugin(ADescriptor,AHost);
     }
     return nullptr;
   }
