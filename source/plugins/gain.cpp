aadac60731d8703684d05de2fa3623f449c39904
tor.helge.skei <tor.helge.skei@gmail.com>
2022-08-06 22:23:14 +0200

    naming tweaks

--- source/plugins/gain.cpp
+++ source/plugins/gain.cpp
@@ -19,7 +19,7 @@
 //
 //----------------------------------------------------------------------
 
-const clap_plugin_descriptor_t myDescriptor = {
+const clap_plugin_descriptor_t gain_descriptor = {
    .clap_version  = CLAP_VERSION,
    .id            = "skei.audio/gain/0",
    .name          = "gain",
@@ -38,35 +38,35 @@ const clap_plugin_descriptor_t myDescriptor = {
 //
 //----------------------------------------------------------------------
 
-class myPlugin
+class gain_plugin
 : public MIP_Plugin {
 
 //------------------------------
 private:
 //------------------------------
 
-  enum myParameterEnums {
+  enum gain_parameter_enums {
     PAR_GAIN = 0,
     PARAM_COUNT
   };
 
-  const clap_param_info_t myParameters[PARAM_COUNT] = {
+  const clap_param_info_t gain_parameters[PARAM_COUNT] = {
     { PAR_GAIN, CLAP_PARAM_IS_AUTOMATABLE, nullptr, "Gain", "", 0, 1, 0 }
   };
 
-  const clap_audio_port_info_t myAudioInputPorts[1] = {
+  const clap_audio_port_info_t gain_audioInputPorts[1] = {
     { 0, "audio in 1", CLAP_AUDIO_PORT_IS_MAIN, 2, CLAP_PORT_STEREO, CLAP_INVALID_ID }
   };
 
-  const clap_audio_port_info_t myAudioOutputPorts[1] = {
+  const clap_audio_port_info_t gain_audioOutputPorts[1] = {
     { 0, "audio out 1", CLAP_AUDIO_PORT_IS_MAIN, 2, CLAP_PORT_STEREO, CLAP_INVALID_ID }
   };
 
-  const clap_note_port_info_t myNoteInputPorts[1] = {
+  const clap_note_port_info_t gain_noteInputPorts[1] = {
     { 0, CLAP_NOTE_DIALECT_CLAP, CLAP_NOTE_DIALECT_CLAP, "note in 1" }
   };
 
-  const clap_note_port_info_t myNoteOutputPorts[1] = {
+  const clap_note_port_info_t gain_noteOutputPorts[1] = {
     { 0, CLAP_NOTE_DIALECT_CLAP, CLAP_NOTE_DIALECT_CLAP, "note out 1" }
   };
 
@@ -78,7 +78,7 @@ private:
 public:
 //------------------------------
 
-  myPlugin(const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost)
+  gain_plugin(const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost)
   : MIP_Plugin(ADescriptor,AHost) {
     MEditorWidth = 420;
     MEditorHeight = 620;
@@ -86,7 +86,7 @@ public:
 
   //----------
 
-  virtual ~myPlugin() {
+  virtual ~gain_plugin() {
   }
 
 //------------------------------
@@ -94,12 +94,12 @@ public: // plugin
 //------------------------------
 
   bool init() final {
-    appendAudioInputPort( &myAudioInputPorts[0] );
-    appendAudioOutputPort(&myAudioOutputPorts[0]);
-    appendNoteInputPort(  &myNoteInputPorts[0]  );
-    appendNoteOutputPort( &myNoteOutputPorts[0] );
+    appendAudioInputPort( &gain_audioInputPorts[0] );
+    appendAudioOutputPort(&gain_audioOutputPorts[0]);
+    appendNoteInputPort(  &gain_noteInputPorts[0]  );
+    appendNoteOutputPort( &gain_noteOutputPorts[0] );
     for (uint32_t i=0; i<PARAM_COUNT; i++) {
-      appendParameter( new MIP_Parameter(&myParameters[i]) );
+      appendParameter( new MIP_Parameter(&gain_parameters[i]) );
     }
     return true;
   }
@@ -123,7 +123,8 @@ public: // plugin
     float* out1 = process->audio_outputs[0].data32[1];
     for (uint32_t i=0; i<len; i++) {
       *out0++ = *in0++ * par_gain;
-      *out1++ = *in1++ * par_gain;    }
+      *out1++ = *in1++ * par_gain;
+    }
   }
 
 //------------------------------
@@ -144,9 +145,10 @@ public: // gui
     gain_knob->Layout.aspectRatio = (4.0 / 6.0);
     background->appendChildWidget(gain_knob);
     MEditor->connectWidget(MParameters[0],gain_knob);
-
     return true;
   }
+
+  //----------
 
 };
 
@@ -166,14 +168,14 @@ public: // gui
 //----------
 
 void MIP_Register(MIP_Registry* ARegistry) {
-  ARegistry->appendDescriptor(&myDescriptor);
+  ARegistry->appendDescriptor(&gain_descriptor);
 };
 
 //----------
 
 MIP_Plugin* MIP_CreatePlugin(uint32_t AIndex, const clap_plugin_descriptor_t* ADescriptor, const clap_host_t* AHost) {
   if (AIndex == 0) {
-    return new myPlugin(ADescriptor,AHost);
+    return new gain_plugin(ADescriptor,AHost);
   }
   return nullptr;
 }
