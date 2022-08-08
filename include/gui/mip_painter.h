2aa5ac140016818ce0591235d04d2e6e7718900a
tor.helge.skei <tor.helge.skei@gmail.com>
2022-08-08 18:23:29 +0200

    preparations for widget clipping hierarchy

--- include/gui/mip_painter.h
+++ include/gui/mip_painter.h
@@ -25,6 +25,13 @@
   typedef MIP_NanoVGPainter MIP_ImplementedPainter;
 #endif
 
+//----------
+
+#if 0
+#define MIP_RECT_STACK_SIZE 1024
+typedef MIP_Stack<MIP_DRect,MIP_RECT_STACK_SIZE> MIP_DRectStack;
+#endif // 0
+
 //----------------------------------------------------------------------
 //
 //
@@ -35,9 +42,13 @@ class MIP_Painter
 : public MIP_ImplementedPainter {
 
 //------------------------------
-protected:
+private:
 //------------------------------
 
+  #if 0
+  MIP_DRect      MClipRect;
+  MIP_DRectStack  MClipStack;
+  #endif
 
 //------------------------------
 public:
@@ -45,12 +56,73 @@ public:
 
   MIP_Painter(MIP_Drawable* ATarget, MIP_Drawable* ASource)
   : MIP_ImplementedPainter(ATarget,ASource) {
+
+    #if 0
+    MClipRect = MIP_FRect( ATarget->getWidth()-1, ATarget->getHeight()-1 );
+    #endif
+
   }
 
   //----------
 
   virtual ~MIP_Painter() {
   }
+
+//------------------------------
+public: // clipping
+//------------------------------
+
+  #if 0
+
+  /*
+    - push current clip rect
+    - set new clip rect
+  */
+
+  void pushClip(MIP_FRect ARect) {
+    //MIP_Print("ARect = %.2f,%.2f,%.2f,%.2f\n",ARect.x,ARect.y,ARect.w,ARect.h);
+    //MIP_Print("depth: %i\n",MClipStack.getNumItems());
+    MClipStack.push(MClipRect);
+    MClipRect = ARect;
+    //resetClip();
+    setClip(MClipRect);
+  }
+
+  //----------
+
+  /*
+    - pop rect
+    - set clip rect to popped rect
+  */
+
+  MIP_FRect popClip() {
+    MClipRect = MClipStack.pop();
+    //MIP_Print("MClipRect = %.2f,%.2f,%.2f,%.2f\n",MClipRect.x,MClipRect.y,MClipRect.w,MClipRect.h);
+    //resetClip();
+    setClip(MClipRect);
+    return MClipRect;
+  }
+
+  //----------
+
+  void resetClipStack() {
+    MClipStack.reset();
+  }
+
+  //----------
+
+  void setClipRect(MIP_FRect ARect) {
+    MClipRect = ARect;
+    //MIP_Print("ARect = %.2f,%.2f,%.2f,%.2f\n",MClipRect.x,MClipRect.y,MClipRect.w,MClipRect.h);
+  }
+
+  //----------
+
+  MIP_FRect getClipRect() {
+    return MClipRect;
+  }
+
+  #endif // 0
 
 //------------------------------
 public:
