e09a405ceef805b8a0e4d26a9cef776bde089c51
tor.helge.skei <tor.helge.skei@gmail.com>
2022-08-16 04:43:59 +0200

    don't default to including vst2/3 wrappers

--- source/plugins/template.cpp
+++ source/plugins/template.cpp
@@ -210,8 +210,8 @@ public: // gui
   #include "plugin/mip_registry.h"
   #include "plugin/clap/mip_clap_entry.h"
   #include "plugin/exe/mip_exe_entry.h"
-  #include "plugin/vst2/mip_vst2_entry.h"
-  #include "plugin/vst3/mip_vst3_entry.h"
+  //#include "plugin/vst2/mip_vst2_entry.h"
+  //#include "plugin/vst3/mip_vst3_entry.h"
 
   //----------
 
