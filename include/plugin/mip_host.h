4d59b651d0db7625c99cf9d1101b6ebfb183b1dd
tor.helge.skei <tor.helge.skei@gmail.com>
2022-10-10 18:30:17 +0200

    initial host class (from plugin perspective)

---
+++ include/plugin/mip_host.h
@@ -0,0 +1,51 @@
+#ifndef mip_host_included
+#define mip_host_included
+//----------------------------------------------------------------------
+
+// host from the plugins perspective..
+
+/*
+class MIP_Host {
+
+//------------------------------
+public:
+//------------------------------
+
+  MIP_Host() {
+  }
+
+  //----------
+
+  virtual ~MIP_Host() {
+  }
+
+//------------------------------
+public:
+//------------------------------
+
+  virtual const char* getName() {
+    return "unknown";
+  }
+
+  //----------
+
+  virtual const char* getVersion() {
+    return "0.0.0";
+  }
+
+  //----------
+
+  virtual void updateParameter(uint32_t AIndex, double AValue) {
+  }
+
+  //----------
+
+  virtual void resizeWindow(uint32_t AWidth, uint32_t AHeight) {
+  }
+
+};
+
+*/
+
+//----------------------------------------------------------------------
+#endif
